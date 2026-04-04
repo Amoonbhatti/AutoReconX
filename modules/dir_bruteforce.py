1fe0f75a573b7abbfa04a53f7c3a0074ad0d5e10
Amoon Bhatti <amoonsaab@gmail.com>
2026-04-05 03:47:10 +0500

    fixed author info

--- modules/dir_bruteforce.py
+++ modules/dir_bruteforce.py
@@ -1,18 +1,16 @@
-import os
+def run_dir_bruteforce(target, wordlist):
+    import subprocess
+    import os
 
-def run_dir_bruteforce(target):
     print(f"[+] Starting directory brute force on {target}")
 
-    url = f"http://{target}"
-    wordlist = "wordlist.txt"  # ya apni path
+    cmd = f"gobuster dir -u http://{target} -w {wordlist} -q"
 
-    output_path = f"results/{target}/directories.txt"
+    result = subprocess.getoutput(cmd)
 
-    # folder ensure karo
     os.makedirs(f"results/{target}", exist_ok=True)
 
-    command = f"gobuster dir -u {url} -w {wordlist} -o {output_path}"
+    with open(f"results/{target}/directories.txt", "w") as f:
+        f.write(result)
 
-    os.system(command)
-
-    print(f"[+] Directory scan saved to {output_path}")
+    return result
